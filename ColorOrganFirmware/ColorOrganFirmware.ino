aa754fc68585a3afc841489552a9c7cb32c87267
Matt Z <mzenthoefer@gmail.com>
2024-11-03 10:05:34 -0700

    best the color organ has ever looked in the garage!

--- ColorOrganFirmware/ColorOrganFirmware.ino
+++ ColorOrganFirmware/ColorOrganFirmware.ino
@@ -40,10 +40,13 @@ void setup() {
     for (freq_amp = 0; freq_amp<7; freq_amp++)
     {
       analogRead(DC_Left);//must be here or you get jitter in first analog input read
+      //Serial.print(String(analogRead(DC_Left)*input_gain)+"\t");
+      //Serial.print(String(analogRead(DC_Right)*input_gain)+"\t");
    
       digitalWrite(STROBE, HIGH);
       digitalWrite(STROBE, LOW);
      }
+     //Serial.println();  
   }
 }
  
@@ -54,11 +57,12 @@ void loop() {
  
 void Read_Frequencies(){
   //Read frequencies for each band
+  
   for (freq_amp = 0; freq_amp<7; freq_amp++)
   {
     analogRead(DC_Left);//must be here or you get jitter in first analog input read
     
-    Frequencies_Left[freq_amp] = analogRead(DC_Left);
+    Frequencies_Left[freq_amp] = analogRead(DC_Left)*1.08;
    
     digitalWrite(STROBE, HIGH);
     digitalWrite(STROBE, LOW);
@@ -68,7 +72,7 @@ void Read_Frequencies(){
   {
     analogRead(DC_Right);//must be here or you get jitter in first analog input read
      
-    Frequencies_Right[freq_amp] = analogRead(DC_Right);
+    Frequencies_Right[freq_amp] = analogRead(DC_Right)*1.08;
    
     digitalWrite(STROBE, HIGH);
     digitalWrite(STROBE, LOW);
@@ -97,9 +101,19 @@ void Set_Leds(){
     {
       //do nothing
     } 
-    output_value=(pow (2.71828, (value/275))/.01)-100;
-    
+    //output_value=4*pow (2.71828, (value/123.2));
+    //output_value=(pow (2.71828, (value/275))/.01)-100;
+    if(value > 118){
+      output_value = 1.6*value;
+    } else {
+      output_value = 0;
+    }
+    //Serial.println("output value is: "+String(output_value)+" and value is: "+String(value));
     set_led(i, output_value);  
+
+    //Serial.println("PWM channel: "+String(i));
+    //Serial.println("inputvalue: "+String(value));
+    //Serial.println("outputvalue: "+String(output_value));
   }
 
   Tlc.update();
@@ -110,6 +124,11 @@ int set_led(int pwm_channel, int output_value)
     if(output_value < 2){
       output_value=0;
     }
+
+    //    int scale_factor = 1.5;
+    //    if(pwm_channel == 0 || pwm_channel == 7){
+    //      scale_factor = 2;
+    //    }
 
     Tlc.set(pwm_channel,output_value);  //was times scale factor computed above
 }
